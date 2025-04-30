67c2c4b470e74c4db185ff88e549ebbc8f8376d2
Xander van der Goot <xandervandergoot@gmail.com>
2025-04-30 19:55:33 +0800

    Add interleaved_4

--- crates/block-multiplier-codegen/src/main.rs
+++ crates/block-multiplier-codegen/src/main.rs
@@ -3,9 +3,16 @@ use hla::builder::{Interleaving, build_inline};
 
 fn main() {
     build_inline(
-        "./asm/montgomery_interleaved.s",
+        "./asm/montgomery_interleaved_3.s",
         Interleaving::par(
             Interleaving::single(setup_montgomery),
+            Interleaving::single(setup_single_step_simd),
+        ),
+    );
+    build_inline(
+        "./asm/montgomery_interleaved_4.s",
+        Interleaving::par(
+            Interleaving::seq(vec![setup_montgomery, setup_montgomery]),
             Interleaving::single(setup_single_step_simd),
         ),
     );
